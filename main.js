fca0d40b1e894a79241a226ed609df48967dc947
eelok <eelok.ru>
2018-04-07 19:31:28 +0200

    make some changes

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let result = 0;
-var result_button = document.getElementById('result');
-var plus_button = document.getElementById('plus');
-var minus_button = ducument.getElementById('minus');
-var multiply_button = document.getElementById('multiply');
-var divide_button = document.getElementById('divide');
-var equally_button = ducument.getElementById('equally');
-var pers_button = ducument.getElementById('pers');
-var del_button = ducument.getElementById('del');
+// var result_button = document.getElementById('result');
+// var plus_button = document.getElementById('plus');
+// var minus_button = ducument.getElementById('minus');
+// var multiply_button = document.getElementById('multiply');
+// var divide_button = document.getElementById('divide');
+// var equally_button = ducument.getElementById('equally');
+// var pers_button = ducument.getElementById('pers');
+// var del_button = ducument.getElementById('del');
+
+var buttons = document.querySelector('.btn');
