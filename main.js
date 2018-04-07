8f852bad8227c02069661a73359e59fcf3812a0a
wassertim <tim@digitalband.ru>
2018-04-07 20:13:21 +0200

    add event listeners for number buttons and output result

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let result = 0;
-// var result_button = document.getElementById('result');
 // var plus_button = document.getElementById('plus');
 // var minus_button = ducument.getElementById('minus');
 // var multiply_button = document.getElementById('multiply');
 // var divide_button = document.getElementById('divide');
 // var equally_button = ducument.getElementById('equally');
-// var pers_button = ducument.getElementById('pers');
-// var del_button = ducument.getElementById('del');
+// var pers_button = ducument.getElementById('pers');    
+var delButton = document.getElementById('del');
+var resultElement = document.querySelector('#result');
+var numberButtons = document.querySelectorAll('.number');
 
-var buttons = document.querySelector('.btn');
+numberButtons.forEach(function(button) {    
+    button.addEventListener('click', function(event) {
+        var resultValue = parseInt(resultElement.innerHTML, 10);
+        if (resultValue === 0) {
+            resultElement.innerHTML = event.target.value;
+        } else {
+            resultElement.innerHTML += event.target.value;
+        }
+    });
+});
+delButton.addEventListener('click', function() {
+    resultElement.innerHTML = 0;
+});
